81f895518acb33d28676303ff19e5d7348de9a85
Aaron Dewes <aaron.dewes@web.de>
2021-10-18 22:58:47 +0100

    Dark mode fixes & allow custom app icons/images

--- src/views/AppStoreApp.vue
+++ src/views/AppStoreApp.vue
@@ -31,7 +31,7 @@
           <div class="d-block">
             <img
               class="app-icon app-icon-lg me-2 me-sm-3 align-self-top"
-              :src="`https://getumbrel.github.io/umbrel-apps-gallery/${app.id}/icon.svg`"
+              :src="app.imageUrl || `https://getumbrel.github.io/umbrel-apps-gallery/${app.id}/icon.svg`"
             />
           </div>
           <div>
@@ -124,7 +124,7 @@
         v-for="image in app.gallery"
         class="app-gallery-screen me-3"
         :key="image"
-        :src="`https://getumbrel.github.io/umbrel-apps-gallery/${app.id}/${image}`"
+        :src="image.startsWith('http') ? image : `https://getumbrel.github.io/umbrel-apps-gallery/${app.id}/${image}`"
       />
       <div class="d-block" style="padding: 1px"></div>
     </div>
