594c45a9d5b00442f990f4066ceded4056b94f2d
Aaron Dewes <aaron.dewes@protonmail.com>
2021-12-10 21:21:02 +0100

    Target esnext so Vite doesn't break the SDK

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "esnext",
+  },
   resolve: { alias: { "@": "/src", vue: "@vue/compat" } },
   plugins: [vue()],
 });
