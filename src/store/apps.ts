387beac254388021cff637ffc308ebc684fd183f
AaronDewes <aaron.dewes@protonmail.com>
2022-06-17 06:07:01 +0000

    Use cleaner getUpdateChannel

--- src/store/apps.ts
+++ src/store/apps.ts
@@ -1,50 +1,10 @@
 import {defineStore} from 'pinia';
+import type {app as appType} from '@runcitadel/sdk';
+
 import useSdkStore from './sdk';
 
-/** A dependency an app could have */
-export type Dependency = 'bitcoind' | 'electrum' | 'lnd' | 'c-lightning';
-/**
- * Defines an app
- */
-export type app = {
-  /** The id of the app, the name as a simple string without spaces */
+export type app = appType & {
   id: string;
-  /** A category for the app, used for grouping apps on the dashboard */
-  category: string;
-  /** The name of the app */
-  name: string;
-  /** The version of the app */
-  version: string;
-  /** A One line description of the app (max 50 characters) */
-  tagline: string;
-  /** A longer description of the app (50 to 200 words) */
-  description: string;
-  /** The person(s) who created the app */
-  developer?: string;
-  /** The person(s) who created the app */
-  developers?: Record<string, string>;
-  /** The dependencies of the app */
-  dependencies: (Dependency | Dependency[])[];
-  /** The url to the app's Git repository */
-  repo: string | Record<string, string>;
-  /** The url to the app's support website/chat */
-  support: string;
-  /** The port the app's web UI uses */
-  port: number;
-  /** A list of links to app promotional images, if no domain is provided, https://runcitadel.github.io/old-apps-gallery/${app.id}/ will be put in front of the path */
-  gallery: string[];
-  /** The path of the app the open button should open */
-  path: string;
-  /** The app's default password */
-  defaultPassword: string;
-  torOnly?: boolean;
-  /** Automatically added */
-  hiddenService?: string;
-  /** Automatically added */
-  installed?: boolean;
-  /** Automatically added */
-  compatible: boolean;
-  icon?: string;
 };
 
 export interface State {
@@ -70,14 +30,14 @@ export default defineStore('apps', {
     async getInstalledApps() {
       const installedApps = await this.sdkStore.citadel.manager.apps.list(true);
       if (installedApps) {
-        this.installed = installedApps;
+        this.installed = installedApps.apps as app[];
       }
     },
     async getAppStore() {
       this.getInstalledApps();
       const appStore = await this.sdkStore.citadel.manager.apps.list();
       if (appStore) {
-        this.store = appStore;
+        this.store = appStore.apps as app[];
       }
     },
     async uninstall(appId: string) {
@@ -86,7 +46,7 @@ export default defineStore('apps', {
 
       const poll = window.setInterval(async () => {
         await this.getInstalledApps();
-        const index = this.installed.findIndex((app) => app.id === appId);
+        const index = this.installed.findIndex((app) => app?.id === appId);
         if (index === -1) {
           this.uninstalling.splice(this.uninstalling.indexOf(appId), 1);
           window.clearInterval(poll);
@@ -99,7 +59,7 @@ export default defineStore('apps', {
 
       const poll = window.setInterval(async () => {
         await this.getInstalledApps();
-        const index = this.installed.findIndex((app) => app.id === appId);
+        const index = this.installed.findIndex((app) => app?.id === appId);
         if (index !== -1) {
           this.installing.splice(this.installing.indexOf(appId), 1);
           window.clearInterval(poll);
