37f47baa3bec525a0f179c7a93d705c35f458179
Philipp Walter <philippwalter@pm.me>
2022-06-19 16:52:06 +0200

    fix: sdk import syntax

--- src/store/sdk.ts
+++ src/store/sdk.ts
@@ -1,4 +1,4 @@
-import Citadel from '../../node_modules/@runcitadel/sdk/dist/index.js';
+import Citadel from '@runcitadel/sdk';
 import {defineStore} from 'pinia';
 import useUserStore from './user';
 
