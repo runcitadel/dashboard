2c47de8a71d0707ee23d8d064019af871908f9a1
Aaron Dewes <aaron@runcitadel.space>
2023-05-14 17:08:21 +0000

    Simplify Nirvati page

--- src/views/Nirvati.vue
+++ src/views/Nirvati.vue
@@ -13,8 +13,11 @@
     >
       <div class="px-3 px-lg-4 pb-3">
         Nirvati is an attempt to fix various issues Citadel has, and increase
-        the amount of users that can use Citadel. You can try Nirvati from the
-        <a href="/app-store/app/nirvati">Nirvati</a> app in the app store.
+        the amount of users that can use Citadel.
+
+        Nirvati is coming soon, and we plan to launch the first preview in June.
+
+        Check <a href="https://nirvati.org">nirvati.org</a> to learn more.
       </div>
     </div>
   </div>
